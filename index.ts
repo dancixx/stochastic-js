d7d4d130349d062b8d3292944e7bf1573b69e8c4
dancixx <dancixx@gmail.com>
2022-04-11 12:45:52 +0200

    export fix

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
+import cir from './src/cir';
 import gbm from './src/gbm';
 import heston from './src/heston';
 import ou from './src/ou';
 import {brownianBridge, correlatedWieners, wiener} from './src/wiener';
 
-export {brownianBridge, correlatedWieners, gbm, heston, ou, wiener};
+export {brownianBridge, cir, correlatedWieners, gbm, heston, ou, wiener};
