90f057e96396e93226122a6c32b33a54e54a2178
Daniel Boros <dancixx@gmail.com>
2022-07-25 01:27:17 +0200

    rates, vol models

--- src/stochastic/index.ts
+++ src/stochastic/index.ts
@@ -1,6 +1,7 @@
 import * as diffusions from './diffusions';
 import * as distributions from './distributions';
 import * as noises from './noises';
-import * as models from './models';
+import * as rates from './rates';
+import * as volatility from './volatility';
 
-export {diffusions, distributions, noises, models};
+export {diffusions, distributions, noises, rates, volatility};
